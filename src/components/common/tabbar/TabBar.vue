88301ffd824c7ec730594c23d0fbb2188923de09
shuffblow <625749330@qq.com>
2021-09-20 15:08:34 +0800

    初始化项目

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="tab-bar">
+        <slot></slot>
+   </div>
+</template>
+
+<script>
+export default {
+    name: "TabBar"
+}
+</script>
+
+<style>
+
+
+  #tab-bar{
+    display: flex;
+    background-color: #f6f6f6;
+
+    /* 将tab-bar固定设在底部 */
+    position:fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+
+
+    /* 设置阴影 */
+    box-shadow: 0 -1px 1px rgba(138, 138, 138, 0.2)
+  }
+
+  .tab-bar-item{
+    flex: 1;
+    text-align: center;
+    height: 49px;
+  }
+</style>
